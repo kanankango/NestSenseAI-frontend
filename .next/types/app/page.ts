b13115f39b1f7ec65f85776f67638414392bda22
Aashi-ghub <161113739+Aashi-ghub@users.noreply.github.com>
2025-01-22 12:09:08 +0530

    add webpack hot-update files and update file paths for app components

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\adip2\NestSenseAI-frontend-4\app\page.tsx
+// File: D:\GitHub\NestSenseAI-frontend\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
